204737541028454fe5fbae6880022f458cdbbf36
Shadow Smith <hey@shadowsmith.com>
2022-09-16 11:06:56 -0500

    Add dummy client to Apollo Server and resolve the ping Query

--- clients/apollo-server/index.js
+++ clients/apollo-server/index.js
@@ -1,8 +1,11 @@
+const path = require('path')
+
 const { ApolloServer, gql } = require('apollo-server')
+const grpc = require('@grpc/grpc-js')
+const protoLoader = require('@grpc/proto-loader')
 
 async function main() {
     try {
-        // Initialize server
         const server = getServer()
         const { url } = await server.listen({ port: process.env.PORT || 4002 })
 
@@ -16,19 +19,62 @@ function getServer() {
     const server = new ApolloServer({
         typeDefs: gql`
             type Query {
-                ping: String!
+                ping(message: String!): String!
             }
         `,
         resolvers: {
             Query: {
-                ping: () => {
-                    return 'Pong'
+                ping: (_parent, { message }, { grpcDataSources }, _info) => {
+                    const pingMessage = new Promise((resolve, reject) => {
+                        grpcDataSources.dummy.PingPong({
+                            message
+                        }, (error, result) => {
+                            if (error) {
+                                reject(error)
+                            }
+                    
+                            resolve(result.message)
+                        })
+                    })
+
+                    return pingMessage
                 }
             }
-        }
+        },
+        context: async () => ({
+            grpcDataSources: {
+                dummy: await initializeDummyClient()
+            }
+        })
     })
 
     return server
+}
+
+function initializeDummyClient() {
+    const PROTO_FILE = '../../server/proto/dummy.proto'
+
+    const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE))
+
+    const grpcObj = grpc.loadPackageDefinition(packageDef)
+
+    const client = new grpcObj.dummyPackage.Dummy(
+        '0.0.0.0:4001',
+        grpc.credentials.createInsecure()
+    )
+
+    const deadline = new Date()
+    deadline.setSeconds(deadline.getSeconds() + 5)
+
+    return new Promise((resolve, reject) => {
+        client.waitForReady(deadline, (error) => {
+            if (error) {
+                reject(error)
+            }
+    
+            resolve(client)
+        })
+    })
 }
 
 main()
