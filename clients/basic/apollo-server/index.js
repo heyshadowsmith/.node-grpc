e97df099919e0dec234d16a56b3acbfbca3092a2
Shadow Smith <hey@shadowsmith.com>
2022-10-03 13:04:59 -0500

    Added bounded cache setting to basic Apollo Server

--- clients/basic/apollo-server/index.js
+++ clients/basic/apollo-server/index.js
@@ -45,7 +45,8 @@ function getServer() {
             grpcDataSources: {
                 dummy: await initializeDummyClient()
             }
-        })
+        }),
+        cache: 'bounded'
     })
 
     return server
